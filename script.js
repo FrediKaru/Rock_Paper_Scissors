3bf1a6c68efb4ac0b8036e6261fa7b73798ae665
Fredi Karu <fredikaru@gmail.com>
2022-07-23 01:07:30 +0300

    Included game round counter and score variables.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ returns random item from a "items" list either
 ‘Rock’, ‘Paper’ or ‘Scissors’. 
 */
 
-function gameRound (playerSelection,computerSelection) {
+function playRound (playerSelection,computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
@@ -18,26 +18,54 @@ function gameRound (playerSelection,computerSelection) {
         return("Noone wins, play again!")
 
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerCount = playerCount + 1;
         return("You win! Rock beats scissors");
         
     } else if (playerSelection == "paper" && computerSelection == "rock") {
+        playerCount = playerCount+1;
         return("You win! Paper beats rock");
 
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerCount = playerCount+1;
         return("You win! Paper beats rock");
 
     } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        computerCount = computerCount+1;
         return("You Lose! Rock beats Scissors");
 
     } else if (computerSelection == "paper" && playerSelection == "rock") {
+        computerCount = computerCount+1;
         return("You Lose! Paper beats rock");
 
     } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        computerCount = computerCount+1;
         return("You Lose! Scissors beats paper");
-    }
-     else {
+
+    } else {
         return("Play again!")
      }
 }
 
 /* Function compares opponents players choice with computer choice and declares winner */
+
+function game() {
+    for (let round = 0; round < 5; round++){
+        playRound (prompt("Rock-paper-scissors!", ""),getComputerChoice())
+        console.log("player score is " + playerCount + " and computer score is " + computerCount);
+    }
+
+    if (playerCount > computerCount) {
+        return("Player wins with a score of "+playerCount)
+    } else if (playerCount < computerCount) {
+        return("Computer wins with a score of "+computerCount)
+    } else {
+        return("Noone wins scores are "+ playerCount + " and " +computerCount)
+    }
+}
+
+let playerCount = 0;
+let computerCount = 0;
+
+let playerSelection = "rock";
+
+console.log(game());
