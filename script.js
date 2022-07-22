a53263c7219c84b9851748175955fc7a9e2d1b48
Fredi Karu <fredikaru@gmail.com>
2022-07-22 23:49:27 +0300

    Declared a function for comparing choices.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let items = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
@@ -10,3 +9,32 @@ function getComputerChoice() {
 returns random item from a "items" list either 
 ‘Rock’, ‘Paper’ or ‘Scissors’. 
 */
+
+function gameRound (playerSelection,computerSelection) {
+    if (playerSelection === computerSelection){
+        return("Noone wins, play again!")
+
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return("You win! Rock beats scissors");
+        
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return("You win! Paper beats rock");
+
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return("You win! Paper beats rock");
+
+    } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        return("You Lose! Rock beats Scissors");
+
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        return("You Lose! Paper beats rock");
+
+    } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        return("You Lose! Scissors beats paper");
+    }
+     else {
+        return("Play again!")
+     }
+}
+
+/* Function compares opponents players choice with computer choice and declares winner */
